b19e3fe7f43b2eb21d5b98dc657a97f4be39fc62
Adhiana <adhianamastur48@gmail.com>
2023-01-31 22:16:26 +0700

    finishing simple testing with 80% coverage

--- simple-testing/primeapp/main_test.go
+++ simple-testing/primeapp/main_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"io"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -60,4 +63,77 @@ func Test_prompt(t *testing.T) {
 	if string(out) != "-> " {
 		t.Errorf("unexpected prompt: expected -> but got %s", string(out))
 	}
+}
+
+func Test_intro(t *testing.T) {
+	// save copy of os.Stdout
+	oldStdout := os.Stdout
+
+	// create a read and write pipe
+	r, w, _ := os.Pipe()
+
+	// set os.Stdout to our write pipe
+	os.Stdout = w
+
+	intro()
+
+	// close writer
+	_ = w.Close()
+
+	// reset os.Stdout to oldStdout
+	os.Stdout = oldStdout
+
+	// read the output of prompt function from read pipe
+	out, _ := io.ReadAll(r)
+
+	if !strings.Contains(string(out), "Enter a whole number") {
+		t.Errorf("unexpected intro text; got %s", string(out))
+	}
+}
+
+func Test_checkNumbers(t *testing.T) {
+	tests := []struct {
+		name           string
+		input          string
+		expectedMsg    string
+		expectedIsDone bool
+	}{
+		{"prime", "7", "7 is a prime number!", false},
+		{"not prime", "8", "8 is not a prime number because it is divisible by 2!", false},
+		{"zero", "0", "0 is not prime, by definition!", false},
+		{"one", "1", "1 is not prime, by definition!", false},
+		{"negative number", "-11", "Negative numbers are not prime, by definition!", false},
+		{"not number", "ini text", "Please enter a whole number!", false},
+		{"empty", "", "Please enter a whole number!", false},
+		{"lowercase q", "q", "", true},
+		{"uppercase Q", "Q", "", true},
+	}
+
+	errFmt := "Error '%s'\n\tExpected: %s\n\tGot: %s"
+
+	for _, tt := range tests {
+		input := strings.NewReader(tt.input)
+		reader := bufio.NewScanner(input)
+		msg, isDone := checkNumbers(reader)
+
+		if msg != tt.expectedMsg {
+			t.Errorf(errFmt, tt.name, tt.expectedMsg, msg)
+		}
+
+		if isDone != tt.expectedIsDone {
+			t.Errorf(errFmt, tt.name, tt.expectedIsDone, isDone)
+		}
+	}
+}
+
+func Test_readUserInput(t *testing.T) {
+	doneChan := make(chan bool)
+
+	var stdin bytes.Buffer
+
+	stdin.Write([]byte("1\nq\n")) // press 1 -> enter -> q -> enter
+
+	go readUserInput(&stdin, doneChan)
+	<-doneChan
+	close(doneChan)
 }
