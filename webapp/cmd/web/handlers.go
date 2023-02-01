974eb916553c0aa5390defc54afec3718beca2f0
Adhiana <adhianamastur48@gmail.com>
2023-02-01 09:28:42 +0700

    Simple Web App

---
+++ webapp/cmd/web/handlers.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type TemplateData struct {
+	IP   string
+	Data map[string]any
+}
+
+// render page
+func (app *application) render(w http.ResponseWriter, r *http.Request, tmpl string, data *TemplateData) error {
+	// parse the template from disk
+	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+
+	// execute the template, passing data if any
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
+}
